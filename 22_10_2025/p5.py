0f80296c7b2ba73de20e1b0d1248bafe288c1c39
rehana-2006 <rehanaprijil@gmail.com>
2025-10-22 13:18:13 +0530

    finished

--- 22_10_2025/p5.py
+++ 22_10_2025/p5.py
@@ -9,3 +9,17 @@
 # Password: python123
 # Output:
 # Password is not strong
+
+def passwords(n):
+    special_characters = "!@#$%^&*"
+    if len(n) < 8:
+        print("Password is not strong")
+    else:
+        for char in n:
+            if char in special_characters:
+                print("Password is strong")
+                break
+        else:
+            print("Password is not strong")
+passwords("my@password")
+passwords("python123")
